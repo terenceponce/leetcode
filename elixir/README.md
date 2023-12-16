2ef53d5a13805a659c517e58b5e01c539a517f7f
Terence Ponce <terenceponce@gmail.com>
2023-12-16 14:05:38 +0000

    Integrate GitHub Actions and add Elixir repo (#2)
    This adds a new Elixir project and a GitHub Actions workflow that runs specifically for the Elixir project.

---
+++ elixir/README.md
@@ -0,0 +1,17 @@
+# Leet Code - Elixir
+
+[![elixir ci](https://github.com/terenceponce/leetcode/actions/workflows/elixir_ci.yml/badge.svg)](https://github.com/terenceponce/leetcode/actions/workflows/elixir_ci.yml)
+[![codecov elixir](https://codecov.io/gh/terenceponce/leetcode/graph/badge.svg?token=i7gC0Uuurl&flag=elixir)](https://codecov.io/gh/terenceponce/leetcode)
+
+These are my solutions to LeetCode problems written in Elixir.
+
+All solutions have a corresponding test in [`test/solutions/`](test/solutions)
+
+## Quick Start
+
+```
+git clone git@github.com:terenceponce/leetcode.git
+asdf install
+cd leetcode/elixir
+mix test
+```
---
+++ elixir/mix.exs
@@ -0,0 +1,44 @@
+defmodule LeetCode.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :leet_code,
+      version: "0.1.0",
+      elixir: "1.15.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "1.7.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "0.18.0", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: ["format", "credo"],
+      "lint.ci": ["format --check-formatted", "credo"]
+    ]
+  end
+end
